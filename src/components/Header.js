118df90c6061deaa597db23f5ae738cb736e525e
Shrawon Roka <73011825+shrawon@users.noreply.github.com>
2025-02-26 15:32:13 +0930

    remove hire me

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,10 +13,7 @@ function Header () {
                                 <h2>Hello, I Am </h2>
                                 <h1>Shrawon Roka</h1>
                                 <p>I am an professional react developer</p>
-                                <div className="header_buttons">
-                                    <a href="header" className="btn btn-outline">
-                                        Hire me
-                                    </a>
+                                <div className="header_buttons">   
                                 </div>
                             </div>                       
                         </div>
